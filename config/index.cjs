7114eb4850c9165ae2abe349dc1483215caab432
lzh123321333 <158805438+lzh123321333@users.noreply.github.com>
2024-02-04 16:27:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'orJMe665f-wm7-7vthhNOioFzsLs',
+      id: 'orJMe676EBNZit-duIVAUKo3k0vQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'fea6TOynNkMz8BW4GUrfTpld0mHAIImNJy7xyzfC9eM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
